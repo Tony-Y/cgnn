9acb1a35cdc7721356455df751efd33b8da22087
Tony-Y <11532812+Tony-Y@users.noreply.github.com>
2022-10-02 09:18:00 +0900

    Add max value

--- tools/plot_history.py
+++ tools/plot_history.py
@@ -1,4 +1,4 @@
-#   Copyright 2019 Takenori Yamamoto
+#   Copyright 2019-2022 Takenori Yamamoto
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -13,14 +13,20 @@
 #   limitations under the License.
 """A program that plots a training history."""
 
+import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
+max_value = None
+if len(sys.argv) == 2:
+    max_value = float(sys.argv[1])
+
 df = pd.read_csv("history.csv")
 df["train_rmse"] = df["train_loss"].map(np.sqrt)
 df["val_rmse"] = df["val_loss"].map(np.sqrt)
-ax1 = df.plot(x="epoch", y=["train_mae", "val_mae", "train_rmse", "val_rmse"])
+ax1 = df.plot(x="epoch", y=["train_mae", "val_mae", "train_rmse", "val_rmse"],
+              ylim=(0, max_value))
 ax1.set_ylabel("Error")
 ax1.set_xlabel("Epochs")
 
